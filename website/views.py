b8761eb1fa31f94cf989a0c9455441db62785b90
xzx <goodstar40@yahoo.com>
2024-03-05 19:01:33 +0330

    The selected HTML template was customized

--- website/views.py
+++ website/views.py
@@ -3,15 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index_view(request):
-    return render(request,'index.html')
-
-def about_view(request):
-
-    return render(request,'about.html')
-    
-def contact_view(request):
-
-    return render(request,'contact.html')
+    return render(request,'website/index.html')
     
 
 # Create your views here.
